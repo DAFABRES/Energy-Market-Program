3595dc748e95b0f71ea7622defab6497ab974f0a
DAFABRES <DAFABRES@gmail.com>
2023-04-21 16:35:32 -0500

    Add files via upload

--- extract.py
+++ extract.py
@@ -5,11 +5,26 @@ from IPython.display import display
 df = pd.read_excel('energy market.xlsx')
 
 def purchase(energy_amt, energy_type):
-    new_limit = df['Sales Limit'][energy_type] - energy_amt
-    df['Sales Limit'][energy_type] = new_limit
-    df.to_excel('energy market.xlsx')
+    if df['Available Energy'][energy_type] > 0:
+        new_limit = df['Available Energy'][energy_type] - energy_amt
+        df['Available Energy'][energy_type] = new_limit
+        df.to_excel('energy market.xlsx', index=False)
+    else:
+        print("unable to purchase")
 
-purchase(25, 0)
+def sell(energy_amt, energy_type):
+    if df['Available Energy'][energy_type] < df['Sales Limit'][energy_type]:
+        new_limit = df['Available Energy'][energy_type] + energy_amt
+        df['Available Energy'][energy_type] = new_limit
+        df.to_excel('energy market.xlsx', index=False)
+    else:
+        print("unable to purchase")
+
+
+
+
+purchase(25, 1)
+sell(25, 1)
 
 #display(df['Sales Limit'][0])
 
