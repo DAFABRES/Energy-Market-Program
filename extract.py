8a76a883856c6e47f8d10de39203dda24c927ee3
DAFABRES <DAFABRES@gmail.com>
2023-04-21 15:49:18 -0500

    Add files via upload

--- extract.py
+++ extract.py
@@ -4,5 +4,13 @@ from IPython.display import display
 
 df = pd.read_excel('energy market.xlsx')
 
-display(df)
+def purchase(energy_amt, energy_type):
+    new_limit = df['Sales Limit'][energy_type] - energy_amt
+    df['Sales Limit'][energy_type] = new_limit
+    df.to_excel('energy market.xlsx')
 
+purchase(25, 0)
+
+#display(df['Sales Limit'][0])
+
+display(df)
